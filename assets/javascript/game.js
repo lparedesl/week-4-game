9a00f9b9ad7e5aadb0d1991b55380e8d6a74b4fa
luis paredes <luis paredes>
2017-03-29 11:41:31 -0400

    updated background link

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
-	var gameStarted = false;
-	var gameEnded = false;
+	var gameStarted  = false;
+	var gameEnded    = false;
 	var fightStarted = false;
-	var fightEnded = false;
-	var characters = ["Yoda", "Spacetrooper", "Luke", "Darth_Vader", "R2D2"];
-	var lives = [180, 100, 120, 150, 100];
-	var enemies = [];
+	var fightEnded   = false;
+	var characters   = ["Yoda", "Spacetrooper", "Luke", "Darth_Vader", "R2D2"];
+	var lives        = [180, 100, 120, 150, 100];
+	var enemies      = [];
 	var chosenCharacter;
 	var chosenCharacterLife;
 	var newChosenCharacterLife;
 	var enemy;
 	var enemyLife;
 	var newEnemyLife;
-	var wins = 0;
+	var wins   = 0;
 	var losses = 0;
-	var n = -2;
+	var n      = -2;
 
 	function reset() {
-		gameStarted = false;
-		gameEnded = false;
+		gameStarted  = false;
+		gameEnded    = false;
 		fightStarted = false;
-		fightEnded = false;
-		characters = ["Yoda", "Spacetrooper", "Luke", "Darth_Vader", "R2D2"];
-		lives = [180, 100, 120, 150, 100];
-		enemies = [];
-		newChosenCharacterLife;
-		newEnemyLife;
+		fightEnded   = false;
+		characters   = ["Yoda", "Spacetrooper", "Luke", "Darth_Vader", "R2D2"];
+		lives        = [180, 100, 120, 150, 100];
+		enemies      = [];
+		// newChosenCharacterLife;
+		// newEnemyLife;
 		// wins = 0,
 		// losses = 0,
 		n = -2;
-		$("[id=" + chosenCharacter.attr("charname") + "_life]").text(chosenCharacterLife);
-		$("[id=" + enemy.attr("charname") + "_life]").text(enemyLife);
+		$("[id=" + chosenCharacter.attr("data-charname") + "_life]").text(chosenCharacterLife);
+		$("[id=" + enemy.attr("data-charname") + "_life]").text(enemyLife);
 
 		$("body").css({
 			"background-image": "url(\"assets/images/Background.jpg\")",
@@ -40,26 +40,26 @@ $(document).ready(function() {
 				"display": "none"
 		});
 		for (var i = 0; i < characters.length; i++) {
-			position = $("[charname=" + characters[i] + "]").position();
-			moveLeft = 328 + 228 * i - position.left;
-			moveTop = 400 - position.top;
-			$("[charname=" + characters[i] + "]").attr("class", "character hvr-pulse-grow");
-			$("[charname=" + characters[i] + "]").html("<img src=\"assets/images/" + characters[i] + ".png\">\n<p id=\"" + characters[i] + "\">" + characters[i] + "</p>\n<p id=\"" + characters[i] + "_life\">" + lives[i] + "</p>");
+			position     = $("[data-charname=" + characters[i] + "]").position();
+			moveLeft     = 328 + 228 * i - position.left;
+			moveTop      = 400 - position.top;
+			$("[data-charname=" + characters[i] + "]").attr("class", "character hvr-pulse-grow");
+			$("[data-charname=" + characters[i] + "]").html("<img src=\"assets/images/" + characters[i] + ".png\">\n<p id=\"" + characters[i] + "\">" + characters[i] + "</p>\n<p id=\"" + characters[i] + "_life\">" + lives[i] + "</p>");
 
-			$("[charname=" + characters[i] + "]").animate({
+			$("[data-charname=" + characters[i] + "]").animate({
 				height: "40%"
 			});
-			$("[charname=" + characters[i] + "]").animate({
-				top: "+=" + moveTop,
-				left: "+=" + moveLeft
+			$("[data-charname=" + characters[i] + "]").animate({
+				top  : "+=" + moveTop,
+				left : "+=" + moveLeft
 			});
 
 			$("[id=" + characters[i] + "], [id=" + characters[i] + "_life]").animate({
 				"font-size": "20px"
 			});
-			$("[id=" + characters[i] + "], [id=" + characters[i] + "_life]").css({
-				top: 380,
-				left: 0,
+			$('[id=' + characters[i] + '], [id=' + characters[i] + '_life]').css({
+				top  : 380,
+				left : 0,
 			});
 		}
 		$(".title").animate({
@@ -89,13 +89,13 @@ $(document).ready(function() {
 			var moveLeft = position.left - 784;
 			var moveTop = position.top - 200;
 			chosenCharacter.animate({
-				top: "-=" + moveTop,
-				left: "-=" + moveLeft
+				top  : "-=" + moveTop,
+				left : "-=" + moveLeft
 			});
 			chosenCharacter.removeClass("hvr-pulse-grow");
 
 			// Set character life value
-			var indexChosenCharacter = characters.indexOf(chosenCharacter.attr("charname"));
+			var indexChosenCharacter = characters.indexOf(chosenCharacter.attr("data-charname"));
 			chosenCharacterLife = lives[indexChosenCharacter];
 			newChosenCharacterLife =  chosenCharacterLife;
 
@@ -105,30 +105,30 @@ $(document).ready(function() {
 
 			// Move enemies
 			for (var i = 0; i < characters.length; i++) {
-				$("[charname=" + characters[i] + "]").removeClass("hvr-pulse-grow");
-				$("[charname=" + characters[i] + "]").addClass("hvr-pulse-shrink");
-				position = $("[charname=" + characters[i] + "]").position();
+				$("[data-charname=" + characters[i] + "]").removeClass("hvr-pulse-grow");
+				$("[data-charname=" + characters[i] + "]").addClass("hvr-pulse-shrink");
+				position = $("[data-charname=" + characters[i] + "]").position();
 				moveLeft = 540 + 200 * i - position.left;
 				moveTop = 750 - position.top;
-				$("[charname=" + characters[i] + "]").animate({
+				$("[data-charname=" + characters[i] + "]").animate({
 					height: "20%"
 				});
-				$("[charname=" + characters[i] + "]").animate({
-					top: "+=" + moveTop,
-					left: "+=" + moveLeft
+				$("[data-charname=" + characters[i] + "]").animate({
+					top  : "+=" + moveTop,
+					left : "+=" + moveLeft
 				});
 				$("[id=" + characters[i] + "], [id=" + characters[i] + "_life]").animate({
 					"font-size": "12px"
 				});
 				$("[id=" + characters[i] + "], [id=" + characters[i] + "_life]").css({
-					top: 180,
-					left: -60,
+					top  : 180,
+					left : -60,
 				});
 			}
 			gameStarted = true;
 		}
 		if (fightStarted === false && gameEnded === false) {
-			n++
+			n++;
 		}
 		$("body").css({
 			"background-image": "url(\"assets/images/Fight_Background.jpg\")",
@@ -145,8 +145,8 @@ $(document).ready(function() {
 
 			// Move character to the left
 			if (n === 0) {
-				var position = chosenCharacter.position(),
-					moveLeft = position.left - 200;
+				position = chosenCharacter.position();
+				moveLeft = position.left - 200;
 				chosenCharacter.animate({
 					left: "-=250"
 				});
@@ -159,7 +159,7 @@ $(document).ready(function() {
 			if (n > 0) {
 				(enemies[(n-1)]).html("");
 				newChosenCharacterLife = chosenCharacterLife;
-				$("[id=" + chosenCharacter.attr("charname") + "_life]").text(newChosenCharacterLife);
+				$("[id=" + chosenCharacter.attr("data-charname") + "_life]").text(newChosenCharacterLife);
 			}
 
 			// Move enemy up
@@ -167,21 +167,21 @@ $(document).ready(function() {
 			moveLeft = position.left - 1034;
 			moveTop = position.top - 215;
 			enemy.animate({
-				top: "-=" + moveTop,
-				left: "-=" + moveLeft,
-				height: "40%"
+				top    : "-=" + moveTop,
+				left   : "-=" + moveLeft,
+				height : "40%"
 			});
-			$("[id=" + enemy.attr("charname") + "], [id=" + enemy.attr("charname") + "_life]").animate({
+			$("[id=" + enemy.attr("data-charname") + "], [id=" + enemy.attr("data-charname") + "_life]").animate({
 				"font-size": "20px"
 			});
-			$("[id=" + enemy.attr("charname") + "], [id=" + enemy.attr("charname") + "_life]").css({
-				top: 380,
-				left: 0,
+			$("[id=" + enemy.attr("data-charname") + "], [id=" + enemy.attr("data-charname") + "_life]").css({
+				top  : 380,
+				left : 0,
 			});
 			enemy.removeClass("hvr-pulse-shrink");
 
 			// Set enemy life value
-			var indexChosenEnemy = characters.indexOf(enemy.attr("charname"));
+			var indexChosenEnemy = characters.indexOf(enemy.attr("data-charname"));
 			enemyLife = lives[indexChosenEnemy];
 			newEnemyLife = enemyLife;
 
@@ -191,13 +191,13 @@ $(document).ready(function() {
 
 			// Move other enemies
 			for (var i = 0; i < characters.length; i++) {
-				position = $("[charname=" + characters[i] + "]").position();
-				moveLeft = 640 + 100 * n + 200 * i - position.left;
-				moveTop = 750 - position.top;
-				$("[charname=" + characters[i] + "]").animate({
+				position     = $("[data-charname=" + characters[i] + "]").position();
+				moveLeft     = 640 + 100 * n + 200 * i - position.left;
+				moveTop      = 750 - position.top;
+				$("[data-charname=" + characters[i] + "]").animate({
 					height: "20%"
 				});
-				$("[charname=" + characters[i] + "]").animate({
+				$("[data-charname=" + characters[i] + "]").animate({
 					top: "+=" + moveTop,
 					left: "+=" + moveLeft
 				});
@@ -215,7 +215,7 @@ $(document).ready(function() {
 			$("#wins").text(wins);
 			$("#losses").text(losses);
 			$(".score").css({
-				"display": "inline"
+				'display': 'inline'
 			});
 		}
 	});
@@ -234,15 +234,15 @@ $(document).ready(function() {
 			// Update characters" lives
 			newChosenCharacterLife -= enemyAttack;
 			newEnemyLife -= attack;
-			$(".instructions").html("<p>You attacked "+ enemy.attr("charname") + " for " + attack + " damage</p>\n<p>" + enemy.attr("charname") + " attacked you back for " + enemyAttack + " damage</p>");
+			$(".instructions").html("<p>You attacked "+ enemy.attr("data-charname") + " for " + attack + " damage</p>\n<p>" + enemy.attr("data-charname") + " attacked you back for " + enemyAttack + " damage</p>");
 
 			if (newChosenCharacterLife > 0) {
-				$("[id=" + chosenCharacter.attr("charname") + "_life]").text(newChosenCharacterLife);
+				$("[id=" + chosenCharacter.attr("data-charname") + "_life]").text(newChosenCharacterLife);
 			}
 
 			// User losses
 			else {
-				$("[id=" + chosenCharacter.attr("charname") + "_life]").text(0);
+				$("[id=" + chosenCharacter.attr("data-charname") + "_life]").text(0);
 				$(".instructions").html("<p>You lost! Press Enter to play again...</p>");
 				if (newEnemyLife > 0) {
 					gameEnded = true;
@@ -250,28 +250,28 @@ $(document).ready(function() {
 				}
 				fightEnded = true;
 				fightStarted = false;
-				losses++
+				losses++;
 				$("#wins").text(wins);
 				$("#losses").text(losses);
 			}
 
 			if (newEnemyLife > 0) {
-				$("[id=" + enemy.attr("charname") + "_life]").text(newEnemyLife);
+				$("[id=" + enemy.attr("data-charname") + "_life]").text(newEnemyLife);
 			}
 
 			// User wins
 			else {
-				$("[id=" + enemy.attr("charname") + "_life]").text(0);
+				$("[id=" + enemy.attr("data-charname") + "_life]").text(0);
 				if (enemies.length === 4) {
 					$(".instructions").html("<p>You won! Press Enter to play again...</p>");
 					gameEnded = true;
 					gameStarted = false;
-					wins++
+					wins++;
 					$("#wins").text(wins);
 					$("#losses").text(losses);
 				}
 				else {
-					$(".instructions").html("<p>You defeated " + enemy.attr("charname") + " .</p>\n<p>Your strength has been increased by 40 percent</p>");
+					$(".instructions").html("<p>You defeated " + enemy.attr("data-charname") + " .</p>\n<p>Your strength has been increased by 40 percent</p>");
 				}
 				fightEnded = true;
 				fightStarted = false;
